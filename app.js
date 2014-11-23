fd0b1f55a4e2fd18fc38cc6930a22f5c05a9f0a2
pcrglennon <pcrglennon@gmail.com>
2014-11-23 16:40:18 -0500

    refactor: rename to app.js, restructuring
    Create barebones `todoItem` directive, rename controller to `TodoCtrl`, update to Angular 1.3.3, clean up `index.html`.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-(function() {
-  var app = angular.module('todo-list', [ ]);
+var app = angular.module('app', [])
 
-  app.controller('ListController', function() {
-    this.title = "General Todos";
-    this.items = [
-      {
-        title: "Hang dry-erase board",
-        priority: 4
-      },
-      {
-        title: "Bring box to Salvo",
-        priority: 2
-      },
-      {
-        title: "Go to Gym",
-        priority: 1
-      }
-    ];
-  });
-}) ();
+.controller('TodoCtrl', function($scope) {
+  $scope.title = "General Todos";
+  $scope.items = [
+    { title: "Hang dry-erase board", priority: 4 },
+    { title: "Bring box to Salvo", priority: 2 },
+    { title: "Go to Gym", priority: 1 }
+  ];
+})
+
+.directive('todoItem', function() {
+  return {
+    restrict: 'E',
+    template: '<h4>{{item.title}}</h4>' +
+               '<h5>Priority: {{item.priority}}</h5>' +
+               '<button>Done</button>'
+  }
+})
+
+;
