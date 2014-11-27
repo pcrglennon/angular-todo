19288f9f861c722763b043a52ed869209a894a73
pcrglennon <pcrglennon@gmail.com>
2014-11-27 12:56:08 -0500

    feat: addTask function + cleanup
    * Removed `todoItem` directive
    * Added addTask function, input fields, & specs
    * Fixed breaking bug, `index.html` was looking for JS in wrong folder

--- src/app.js
+++ src/app.js
@@ -2,20 +2,17 @@ var app = angular.module('app', [])
 
 .controller('TodoCtrl', function($scope) {
   $scope.title = "General Todos";
-  $scope.items = [
-    { title: "Hang dry-erase board", priority: 4 },
+  $scope.tasks = [
+    { title: "Hang dry-erase board", priority: 3 },
     { title: "Bring box to Salvo", priority: 2 },
     { title: "Go to Gym", priority: 1 }
   ];
-})
 
-.directive('todoItem', function() {
-  return {
-    restrict: 'E',
-    template: '<h4>{{item.title}}</h4>' +
-               '<h5>Priority: {{item.priority}}</h5>' +
-               '<button>Done</button>'
-  }
+  $scope.priorities = [1, 2, 3];
+
+  $scope.addTask = function() {
+    $scope.tasks.push({title: $scope.newTask.title, priority: $scope.newTask.priority});
+  };
 })
 
 ;
--- src/app.spec.js
+++ src/app.spec.js
@@ -9,8 +9,27 @@ describe('TodoCtrl', function() {
     $controller('TodoCtrl', {$scope: $scope});
   }));
 
-  it('has a list of items', function() {
-    expect($scope.items).toBeDefined();
+  it('has a list of tasks', function() {
+    expect($scope.tasks).toBeDefined();
+  });
+
+  it('has a list of priority levels', function() {
+    expect($scope.priorities).toEqual([1, 2, 3]);
+  })
+
+  describe('addTask', function() {
+
+    beforeEach(function() {
+      $scope.tasks = [];
+      $scope.tasks.push({title: "Write better tests", priority: 1});
+      $scope.tasks.push({title: "Write more tests", priority: 2});
+    })
+
+    it('adds a task to tasks', function() {
+      $scope.newTask = {title: "Write Protractor tests", priority: 2};
+      $scope.addTask();
+      expect($scope.tasks.length).toEqual(3);
+    });
   });
 
 });
